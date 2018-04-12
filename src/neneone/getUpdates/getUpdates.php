0074d241665c9733ef4efc4e86c25ffc6869f342
Enea Dolcini <eneadolcini@gmail.com>
2018-04-12 16:37:04 +0200

    A moment please.

--- src/neneone/getUpdates/getUpdates.php
+++ src/neneone/getUpdates/getUpdates.php
@@ -20,6 +20,7 @@ namespace neneone\getUpdates;
 class getUpdates
 {
     use \neneone\getUpdates\Wrappers\serializedDatabase;
+    use \neneone\getUpdates\Wrappers\loopUpdates;
 
     public function __construct($settings)
     {
@@ -71,49 +72,6 @@ class getUpdates
             $this->settings['updates']['event_handler'] = $function;
         } else {
             throw new \neneone\getUpdates\Exception('L\'EventHandler deve essere una funzione valida!');
-        }
-    }
-
-    public function loopUpdates($fork = false)
-    {
-        $offset = 0;
-        $time = time();
-        \neneone\getUpdates\Logger::log('Starting updates loop...');
-        if (true == $fork) {
-            while (true) {
-                $updates = $this->botAPI->getUpdates(['offset' => $offset]);
-                if (isset($this->settings['db']['serialization_interval'])) {
-                    if ($time + $this->settings['db']['serialization_interval'] <= time()) {
-                        $time = time();
-                        $this->serializeDatabase();
-                    }
-                }
-                foreach ($updates['result'] as $key => $value) {
-                    $update = $updates['result'][$key];
-                    $offset = $update['update_id'] + 1;
-                    if (!pcntl_fork()) {
-                        $this->settings['updates']['event_handler']($update);
-                        exit;
-                    }
-                }
-                $offset = (count($updates['result']) - 1) > 0 ? $updates['result'][count($updates['result']) - 1]['update_id'] + 1 : $offset;
-            }
-        } else {
-            while (true) {
-                $updates = $this->botAPI->getUpdates(['offset' => $offset]);
-                if (isset($this->settings['db']['serialization_interval'])) {
-                    if ($time + $this->settings['db']['serialization_interval'] <= time()) {
-                        $time = time();
-                        $this->serializeDatabase();
-                    }
-                }
-                foreach ($updates['result'] as $key => $value) {
-                    $update = $updates['result'][$key];
-                    $offset = $update['update_id'] + 1;
-                    $this->settings['updates']['event_handler']($update);
-                }
-                $offset = (count($updates['result']) - 1) > 0 ? $updates['result'][count($updates['result']) - 1]['update_id'] + 1 : $offset;
-            }
         }
     }
 }
