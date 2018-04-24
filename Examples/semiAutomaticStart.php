7aa13489e217276ab95eca4c12021bdbe1734f08
Enea Dolcini <eneadolcini@gmail.com>
2018-04-24 21:07:49 +0000

    Apply fixes from StyleCI

--- Examples/semiAutomaticStart.php
+++ Examples/semiAutomaticStart.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * Copyright (C) 2018 Enea Dolcini
+ * This file is part of getUpdates.
+ * getUpdates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * getUpdates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU  Affero General Public License
+ * along with getUpdates.  If not, see http://www.gnu.org/licenses.
+ *
+ */
+
 require_once __DIR__.'/vendor/autoload.php';
 
 $Settings = [
@@ -8,11 +24,9 @@ $Settings = [
 
 $getUpdatesBot = new \neneone\getUpdates\getUpdates($Settings); // Inizializzare la classe
 
-while(true) { // Bisogna verificare per sempre se ci sono nuovi updates
-  while($update = $getUpdates->API->getUpdate()) { // Questa funzione restituisce solo un'update, il primo preso. Ogni volta che viene chiamata restituisce il successivo.
+while (true) { // Bisogna verificare per sempre se ci sono nuovi updates
+  while ($update = $getUpdates->API->getUpdate()) { // Questa funzione restituisce solo un'update, il primo preso. Ogni volta che viene chiamata restituisce il successivo.
     $chatID = $update['message']['chat']['id'];
-    $getUpdatesBot->API->sendMessage($chatID, 'Ciao!');
+      $getUpdatesBot->API->sendMessage($chatID, 'Ciao!');
   }
 }
-
- ?>
