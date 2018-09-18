07d889c1947ce0a78c3066347288cbaf81f58b8d
Enea Dolcini <eneadolcini@gmail.com>
2018-09-18 19:21:53 +0200

    Example Fix

--- Examples/semiAutomaticStart.php
+++ Examples/semiAutomaticStart.php
@@ -22,11 +22,11 @@ $Settings = [
   'token' => '523209487:AAEtYvbeFiGJ2g-2I76c0c1y6EC5NiKW_YQ', // Inserire il token del bot
 ];
 
-$getUpdatesBot = new \neneone\getUpdates\getUpdates($Settings); // Inizializzare la classe
+$getUpdates = new \neneone\getUpdates\getUpdates($Settings); // Inizializzare la classe
 
 while (true) { // Bisogna verificare per sempre se ci sono nuovi updates
   while ($update = $getUpdates->API->getUpdates()) { // Questa funzione restituisce solo un'update, il primo preso. Ogni volta che viene chiamata restituisce il successivo.
     $chatID = $update['message']['chat']['id'];
-      $getUpdatesBot->API->sendMessage($chatID, 'Ciao!');
+      $getUpdates->API->sendMessage($chatID, 'Ciao!');
   }
 }
